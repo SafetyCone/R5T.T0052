9cacff121e4081f4230416024829c4fae20794bb
davidcoats <david@getsafetycone.com>
2021-09-28 16:02:14 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0052.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0052.Construction
+{
+    /// <summary>
+    /// Construction project for {Type Code}-{GUID} strongly-typed GUID types library..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0052/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0052
+{
+    /// <summary>
+    /// {Type Code}-{GUID} strongly-typed GUID types library.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
