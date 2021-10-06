6558a841cf15af282dee9ddb2d77a13cb55a243f
David Coats <david@getsafetycone.com>
2021-10-05 22:23:16 -0700

    Add TypeCodeIdentifier strongly-typed string.

---
+++ source/R5T.T0052/Code/Classes/TypeCodedIdentifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.T0052
+{
+    /// <summary>
+    /// Example: EX-3a0af49f-2f01-43b9-8238-d834452661db as the identity that uniquely identifies and experiment with the "EX" identifier type code.
+    /// </summary>
+    public class TypeCodedIdentifier : TypedString
+    {
+        public TypeCodedIdentifier(string value)
+            : base(value)
+        {
+        }
+    }
+}
