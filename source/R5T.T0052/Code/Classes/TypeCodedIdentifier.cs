f35b1bb031caa0324cb10fd5f3e874096aa30b72
davidcoats <david@getsafetycone.com>
2023-02-24 00:23:00 -0800

    Interim changes.

--- source/R5T.T0052/Code/Classes/TypeCodedIdentifier.cs
+++ source/R5T.T0052/Code/Classes/TypeCodedIdentifier.cs
@@ -2,12 +2,15 @@
 
 using R5T.Stagira;
 
+using R5T.T0150;
+
 
 namespace R5T.T0052
 {
     /// <summary>
-    /// Example: EX-3a0af49f-2f01-43b9-8238-d834452661db as the identity that uniquely identifies and experiment with the "EX" identifier type code.
+    /// Example: EX-3a0af49f-2f01-43b9-8238-d834452661db as the identity that uniquely identifies an experiment with the "EX" identifier type code.
     /// </summary>
+    [StrongTypeMarker]
     public class TypeCodedIdentifier : TypedString
     {
         public TypeCodedIdentifier(string value)
